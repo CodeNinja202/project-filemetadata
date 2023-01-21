73f307e39a60a667516b1383a16ccd96867abdbd
Ross <rosskeene196@gmail.com>
2023-01-21 12:31:37 -0600

    complete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 var app = express();
-
+const { JWT_SECRET } = process.env;
+const { PORT = 3000 } = process.env
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,10 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  })
+})
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
 });
